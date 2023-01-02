454daa7c7791d93859bb9e8b60e280700f271d0e
Howon-Shin <shinhw371@naver.com>
2023-01-02 23:54:59 +0900

    :sparkles: Feat: Add example custom style
    need to customize a lot more

--- src/hooks/useCodemirror.tsx
+++ src/hooks/useCodemirror.tsx
@@ -1,11 +1,32 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { EditorState } from '@codemirror/state';
 import { EditorView } from '@codemirror/view';
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
+import { syntaxHighlighting, HighlightStyle } from '@codemirror/language';
+import { tags } from '@lezer/highlight';
 
 interface useCodemirrorParamsType {
   initialDoc: string;
   setDoc: React.Dispatch<React.SetStateAction<string>>;
 }
+
+const markdownHighlighting = HighlightStyle.define([
+  {
+    tag: tags.heading1,
+    fontSize: '1.6em',
+    fontWeight: 'bold',
+  },
+  {
+    tag: tags.heading2,
+    fontSize: '1.4em',
+    fontWeight: 'bold',
+  },
+  {
+    tag: tags.heading3,
+    fontSize: '1.2em',
+    fontWeight: 'bold',
+  },
+]);
 
 function useCodemirror({ initialDoc, setDoc }: useCodemirrorParamsType) {
   const ref = useRef<HTMLDivElement>(null);
@@ -21,6 +42,11 @@ function useCodemirror({ initialDoc, setDoc }: useCodemirrorParamsType) {
       doc: initialDoc,
       // contentHeight: '100%',
       extensions: [
+        markdown({
+          base: markdownLanguage, // Support GFM
+        }),
+        syntaxHighlighting(markdownHighlighting),
+        EditorView.lineWrapping,
         EditorView.updateListener.of(update => {
           if (update.docChanged) {
             setDoc(update.state.doc.toString());

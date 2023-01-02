7619faed9b8265e6b21d967dfc5fb19a36a357f2
Howon-Shin <shinhw371@naver.com>
2023-01-02 18:13:48 +0900

    :sparkles: Feat: Add markdown previewer

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, createElement, Fragment } from 'react';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse/lib';
+import remarkGfm from 'remark-gfm';
+import remarkRehype from 'remark-rehype';
+import rehypeReact from 'rehype-react/lib';
 
 function Write() {
-  return <div>Write</div>;
+  const [doc, setDoc] = useState('# Hello byome');
+
+  const md: any = unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeReact, { createElement, Fragment })
+    .processSync(doc).result;
+
+  return (
+    <div>
+      <div>{doc}</div>
+      <div>{md}</div>
+    </div>
+  );
 }
 
 export default Write;

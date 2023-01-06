94a1003f2adc5f839dc54f0f2137dd7f7c565036
silky225 <silky225@naver.com>
2023-01-06 10:47:57 +0900

    💄 Design: add UI & style files for Home etc.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Header from '../components/Header';
+import Login from '../components/Login';
+import HomeContents from '../components/HomeContents';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: '#F8F9FA',
+      }}
+    >
+      <Header />
+      <HomeContents />
+    </div>
+  );
 }
 
 export default Home;
--- src/pages/ListsLiked.tsx
+++ src/pages/ListsLiked.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Header from '../components/Header';
+import HomeContents from '../components/HomeContents';
 
 function ListsLiked() {
-  return <div>ListsLiked</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: '#F8F9FA',
+      }}
+    >
+      <Header />
+      <HomeContents />
+    </div>
+  );
 }
 
 export default ListsLiked;

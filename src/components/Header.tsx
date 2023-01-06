1dbd67c5c8dfd5a57b46fc888b1183c7e3ed0b51
silky225 <silky225@naver.com>
2023-01-06 10:36:17 +0900

    💄 Design: add UI & style files for Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,135 @@
-export default function Header() {}
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import search from '../resources/search.png';
+import more from '../resources/more.jpeg';
+
+const cx = classNames.bind(styles);
+
+export default function Header() {
+  const [menuOn, setMenuOn] = useState(false);
+
+  const [logOn, setLogOn] = useState(false);
+
+  const [modalOn, setModalOn] = useState(false);
+
+  const path = useLocation().pathname;
+
+  function useScrollDirection() {
+    const [scrollDirection, setScrollDirection] = useState('up');
+
+    useEffect(() => {
+      let lastScrollY = window.pageYOffset;
+
+      const updateScrollDirection = () => {
+        const scrollY = window.pageYOffset;
+        const direction = scrollY > lastScrollY ? 'down' : 'up';
+        if (
+          direction !== scrollDirection &&
+          (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)
+        ) {
+          setScrollDirection(direction);
+        }
+        lastScrollY = scrollY > 0 ? scrollY : 0;
+      };
+      window.addEventListener('scroll', updateScrollDirection); // add event listener
+      return () => {
+        window.removeEventListener('scroll', updateScrollDirection); // clean up
+      };
+    }, [scrollDirection]);
+
+    return scrollDirection;
+  }
+  const scrollDirection = useScrollDirection();
+
+  function openMenu() {
+    if (menuOn === false) {
+      setMenuOn(true);
+    } else {
+      setMenuOn(false);
+    }
+  }
+
+  function changeModal() {
+    if (modalOn === false) {
+      setModalOn(true);
+    } else {
+      setModalOn(false);
+    }
+  }
+
+  return (
+    <div>
+      <div className={scrollDirection === 'down' ? cx('blind') : cx('header')}>
+        <div className={cx('center')}>
+          <div className={path === '/' ? cx('left') : cx('blind')}>
+            <a href="/">velog</a>
+          </div>
+          <div className={path === '/' ? cx('blind') : cx('left')}>
+            <a href="/">
+              <img
+                src="https://th.bing.com/th/id/R.96f8c2f286f01b640e87b113e630b540?rik=615zqZQiGS8hLQ&riu=http%3a%2f%2fwww.zumalakarregimuseoa.eus%2feu%2f06vimeo.png&ehk=KRybvv%2fa1k3ANJCrkUGMFflBCp4fD6JZTDUTlkXvbRk%3d&risl=&pid=ImgRaw&r=0"
+                alt="velog"
+              />
+            </a>
+            <a href={'/'.concat('')}>id.log</a>
+          </div>
+          <div className={cx('right')}>
+            <a href="/search">
+              <img className={cx('search')} src={search} alt="search" />
+            </a>
+            <a href="/write">
+              <button
+                type="button"
+                className={logOn ? cx('write-button') : cx('blind')}
+              >
+                새 글 작성
+              </button>
+            </a>
+            <button
+              type="button"
+              className={logOn ? cx('blind') : cx('login-button')}
+            >
+              로그인
+            </button>
+            <div>
+              <div
+                className={cx('user')}
+                onClick={openMenu}
+                role="presentation"
+              >
+                <img
+                  className={cx('profile')}
+                  src="https://velog.velcdn.com/images/silky225/profile/f3d11391-6a64-4cf0-9889-46778956d77e/social_profile.png"
+                  alt="profile"
+                />
+                <img className={cx('more')} src={more} alt="more" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={menuOn ? cx('menu') : cx('blind')}>
+          <div className={cx('menu-wrapper')}>
+            <a href="/myvelog">
+              <div>내 벨로그</div>
+            </a>
+            <a href="/saves">
+              <div>임시 글</div>
+            </a>
+            <a href="/lists/liked">
+              <div>읽기 목록</div>
+            </a>
+            <a href="/follows">
+              <div>팔로우 관리</div>
+            </a>
+            <a href="/setting">
+              <div>설정</div>
+            </a>
+            <div>로그아웃</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

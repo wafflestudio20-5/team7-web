cff86419f818a54ba89199011f91121a988eea58
silky225 <silky225@naver.com>
2023-01-13 16:48:25 +0900

    ✨ Feat: add HeaderMoving to Home

--- src/components/HeaderMoving.tsx
+++ src/components/HeaderMoving.tsx
@@ -1,20 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
+import styles from './HeaderMoving.module.scss';
 import search from '../resources/search.png';
 import more from '../resources/more.jpeg';
+import HeaderFilter from './HeaderFilter';
 
 const cx = classNames.bind(styles);
 
 export default function Header() {
   const [menuOn, setMenuOn] = useState(false);
 
-  const [logOn, setLogOn] = useState(true);
+  const [logOn, setLogOn] = useState(false);
 
   const [modalOn, setModalOn] = useState(false);
 
   const path = useLocation().pathname;
+
+  function useScrollDirection() {
+    const [scrollDirection, setScrollDirection] = useState('up');
+
+    useEffect(() => {
+      let lastScrollY = window.pageYOffset;
+
+      const updateScrollDirection = () => {
+        const scrollY = window.pageYOffset;
+        const direction = scrollY > lastScrollY ? 'down' : 'up';
+        if (
+          direction !== scrollDirection &&
+          (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)
+        ) {
+          setScrollDirection(direction);
+        }
+        lastScrollY = scrollY > 0 ? scrollY : 0;
+      };
+      window.addEventListener('scroll', updateScrollDirection);
+      return () => {
+        window.removeEventListener('scroll', updateScrollDirection);
+      };
+    }, [scrollDirection]);
+
+    return scrollDirection;
+  }
+
+  const scrollDirection = useScrollDirection();
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const listenToScroll = () => {
+    const heightToHideFrom = 200;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (winScroll <= heightToHideFrom) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenToScroll);
+    return () => window.removeEventListener('scroll', listenToScroll);
+  }, []);
 
   function openMenu() {
     if (menuOn === false) {
@@ -34,12 +82,16 @@ export default function Header() {
 
   return (
     <div>
-      <div className={cx('header')}>
+      <div
+        className={
+          scrollDirection === 'down' || !isVisible ? cx('blind') : cx('header')
+        }
+      >
         <div className={cx('center')}>
           <div className={path === '/myvelog' ? cx('blind') : cx('left')}>
             <a href="/">velog</a>
           </div>
-          <div className={path === '/myvelog' ? cx('left') : cx('blind')}>
+          <div className={path === '/' ? cx('left') : cx('blind')}>
             <a href="/">
               <img
                 src="https://th.bing.com/th/id/R.96f8c2f286f01b640e87b113e630b540?rik=615zqZQiGS8hLQ&riu=http%3a%2f%2fwww.zumalakarregimuseoa.eus%2feu%2f06vimeo.png&ehk=KRybvv%2fa1k3ANJCrkUGMFflBCp4fD6JZTDUTlkXvbRk%3d&risl=&pid=ImgRaw&r=0"
@@ -102,6 +154,7 @@ export default function Header() {
             </div>
           </div>
         </div>
+        <HeaderFilter />
       </div>
     </div>
   );

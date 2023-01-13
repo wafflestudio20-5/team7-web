0ab1ab16838f06a0037ac636288f1c315cdb389a
silky225 <silky225@naver.com>
2023-01-13 20:29:01 +0900

    💄 Style: Change img file to svg

--- src/components/HeaderFilter.tsx
+++ src/components/HeaderFilter.tsx
@@ -24,8 +24,6 @@ export default function HeaderFilter() {
     setFilterOn(false);
   }
 
-  console.log(path.split('/')[1]);
-
   return (
     <div
       className={
@@ -41,17 +39,31 @@ export default function HeaderFilter() {
           }
         >
           <a href="/" className={path === '/' ? cx('highlight') : ''}>
-            <img
-              src="https://miro.medium.com/fit/c/160/160/1*sNBbZwrZ0NRswY2lwWZ3Lw.png"
-              alt="trend"
-            />
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M16 6l2.29 2.29-4.88 4.88-4-4L2 16.59 3.41 18l6-6 4 4 6.3-6.29L22 12V6z" />
+            </svg>
             트렌딩
           </a>
           <a href="/recent" className={path === '/' ? '' : cx('highlight')}>
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3917/3917267.png"
-              alt="recent"
-            />
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z" />
+            </svg>
             최신
           </a>
         </div>

f88e3b8875dfc164ed731ee1c8155a52146148b0
2-0-is <yuye2002@snu.ac.kr>
2023-01-14 12:40:55 +0900

    :art: modify for cleaner code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const cx = classNames.bind(styles);
 export default function Header() {
   const [menuOn, setMenuOn] = useState(false);
 
-  // const [logOn, setLogOn] = useState(true);
+  const { isLogin } = useLoginValue();
   const { logout } = useLoginSetting();
 
   const [modalOn, setModalOn] = useState(false);
@@ -63,24 +63,20 @@ export default function Header() {
             <a href="/write">
               <button
                 type="button"
-                className={
-                  useLoginValue().isLogin ? cx('write-button') : cx('blind')
-                }
+                className={isLogin ? cx('write-button') : cx('blind')}
               >
                 새 글 작성
               </button>
             </a>
             <button
               type="button"
-              className={
-                useLoginValue().isLogin ? cx('blind') : cx('login-button')
-              }
+              className={isLogin ? cx('blind') : cx('login-button')}
             >
               로그인
             </button>
             <div>
               <div
-                className={useLoginValue().isLogin ? cx('user') : cx('blind')}
+                className={isLogin ? cx('user') : cx('blind')}
                 onClick={openMenu}
                 role="presentation"
               >

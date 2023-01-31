597341dddd3d2d4ff3382ad03ccd5a29fa87f92d
Howon-Shin <shinhw371@naver.com>
2023-01-31 23:16:09 +0900

    :truck: Rename: Change axios defaults code position

--- src/contexts/LoginProvider.tsx
+++ src/contexts/LoginProvider.tsx
@@ -34,6 +34,12 @@ const initialSetting: loginSetting = {
 const loginValueContext = createContext<loginValue>(initialValue);
 const loginSettingContext = createContext<loginSetting>(initialSetting);
 
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'development' ? '' : 'https://api.7elog.store';
+axios.defaults.withCredentials = true;
+
 export default function LoginProvider({
   children,
 }: {
@@ -46,11 +52,6 @@ export default function LoginProvider({
   });
 
   const navigate = useNavigate();
-  axios.defaults.xsrfCookieName = 'csrftoken';
-  axios.defaults.xsrfHeaderName = 'X-CSRFToken';
-  axios.defaults.baseURL =
-    process.env.NODE_ENV === 'development' ? '' : 'https://api.7elog.store';
-  axios.defaults.withCredentials = true;
 
   const setting = useMemo(
     () => ({

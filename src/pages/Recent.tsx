3cfae67607f4248f3f9dc3653fda50722b0e73c8
silky225 <silky225@naver.com>
2023-02-03 15:57:30 +0900

    ✨ Feat: add infinite scroll for Home/Recent

--- src/pages/Recent.tsx
+++ src/pages/Recent.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Header from '../components/Header';
-import HeaderMoving from '../components/HeaderMoving';
 import HomeContents from '../components/HomeContents';
 import { post } from '../contexts/types';
 
@@ -9,20 +7,52 @@ function Recent() {
   const tempPosts: post[] = [];
   const [posts, setPosts] = useState(tempPosts);
 
-  const getVelogRecent = async () => {
-    try {
-      const response = await axios.get('/api/v1/velog/recent');
-      setPosts(response.data.results);
-    } catch (error) {
-      console.log(error);
+  const [target, setTarget] = useState<HTMLElement | null>(null);
+  let pageNumber: number = 1;
+
+  const [loading, setLoading] = useState(false);
+
+  async function getVelog(
+    [entry]: IntersectionObserverEntry[],
+    observer: {
+      unobserve: (arg0: Element) => void;
+      observe: (arg0: Element) => void;
     }
-  };
+  ) {
+    if ((pageNumber === 1 || entry.isIntersecting) && !loading) {
+      try {
+        observer.unobserve(entry.target);
+        setLoading(true);
+        const response = await axios.get(
+          `/api/v1/velog/recent?page=${pageNumber}`
+        );
+        setPosts(posts => posts.concat(response.data.results));
+        pageNumber += 1;
+        setLoading(false);
+        observer.observe(entry.target);
+      } catch (error: Error | any) {
+        console.log(error);
+      }
+    }
+  }
 
   useEffect(() => {
-    getVelogRecent();
-  }, []);
+    let observer: IntersectionObserver;
+    if (target) {
+      observer = new IntersectionObserver(getVelog, {
+        threshold: 0.5,
+      });
+      observer.observe(target);
+    }
+    return () => observer && observer.disconnect();
+  }, [target]);
 
-  return <HomeContents posts={posts} />;
+  return (
+    <>
+      <HomeContents posts={posts} />
+      <div ref={setTarget} />
+    </>
+  );
 }
 
 export default Recent;

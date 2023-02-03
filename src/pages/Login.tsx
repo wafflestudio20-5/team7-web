2ead5d9a0945b84c200064a49ad26c8b4adeb8bb
silky225 <silky225@naver.com>
2023-02-03 15:56:22 +0900

    📝 Docs: restrict wrong access

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import LoginModal from '../components/LoginModal';
+import { useLoginValue } from '../contexts/LoginProvider';
+import { showToast } from '../components/Toast';
 
 function Login() {
+  const navigate = useNavigate();
+  const { isLogin } = useLoginValue();
+  useEffect(() => {
+    function checkLogin() {
+      if (isLogin) {
+        showToast({ type: 'error', message: '잘못된 접근입니다' });
+        navigate('/');
+      }
+    }
+    checkLogin();
+  }, [isLogin]);
   return <LoginModal />;
 }
 
--- src/pages/LoginProcess.tsx
+++ src/pages/LoginProcess.tsx
@@ -1,15 +1,39 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useLoginSetting } from '../contexts/LoginProvider';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLoginValue, useLoginSetting } from '../contexts/LoginProvider';
+import { showToast } from '../components/Toast';
 
 function LoginProcess() {
+  const navigate = useNavigate();
+  const { isLogin } = useLoginValue();
+  useEffect(() => {
+    function checkLogin() {
+      if (isLogin) {
+        showToast({ type: 'error', message: '잘못된 접근입니다' });
+        navigate('/');
+      }
+    }
+    checkLogin();
+  }, [isLogin]);
+
   const { googleFinish } = useLoginSetting();
 
   useEffect(() => {
     googleFinish();
   }, []);
 
-  return <div>Login Process</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '20%',
+      }}
+    >
+      <div style={{ fontSize: '3rem', color: '#12b886' }}>로그인 시도중..</div>
+    </div>
+  );
 }
 
 export default LoginProcess;
